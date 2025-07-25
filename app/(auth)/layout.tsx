37b950d750a8a1ed741124b815a31cca0a6466f8
rhixecompany <rhixecompany@gmail.com>
2025-07-25 22:37:30 +0100

    implement initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+
+import Image from "next/image";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+        <div>
+          <Image
+            src="/icons/auth-image.svg"
+            alt="Auth image"
+            width={500}
+            height={500}
+            className="rounded-l-xl object-contain"
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <h2>Welcome to sign in</h2>
+    </section>
+  );
+};
+
+export default SignIn;
