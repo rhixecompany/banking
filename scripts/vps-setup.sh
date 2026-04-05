196cdc7a1648036ae1327887983576e9f312e329
rhixecompany <rhixecompany@gmail.com>
2026-04-05 07:45:54 +0100

    fix: vps-setup.sh v1.3 - clear CDN cache

--- scripts/vps-setup.sh
+++ scripts/vps-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# vps-setup.sh - Automated Banking App VPS Setup v1.2
+# vps-setup.sh - Automated Banking App VPS Setup v1.3
 # Usage: curl -sSL https://raw.githubusercontent.com/rhixecompany/banking/main/scripts/vps-setup.sh | bash
 
 set -e
