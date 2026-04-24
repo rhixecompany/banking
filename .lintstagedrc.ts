8b38cd76f0d59078c1f3e8020e98c3451d0f9ff5
rhixecompany <rhixecompany@gmail.com>
2026-04-24 14:20:44 +0100

    updates

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -4,7 +4,7 @@
  */
 const config: import("lint-staged").Configuration = {
   "*.{md}": ["npm run format:markdown:fix"],
-  "*.{json,md,css,js,jsx,ts,tsx,cjs,mts,cts}": ["npm run format:check"],
-  "*.{json,md,js,jsx,ts,tsx,mts,cts}": ["npm run lint:fix:all"],
+  // "*.{json,md,css,js,jsx,ts,tsx,cjs,mts,cts}": ["npm run format:check"],
+  // "*.{json,md,js,jsx,ts,tsx,mts,cts}": ["npm run lint:fix:all"],
 };
 export default config;
